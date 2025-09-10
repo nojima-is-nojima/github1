cc6f8b02741bee12c71435b54f06c2b22adeee0c
nijima <marco.ny.1109@gmail.com>
2025-09-10 17:31:47 +0900

    データベース前期課題

---
+++ aaa.py
@@ -0,0 +1,45 @@
+age = 20
+
+if age >= 20 :
+    print("20歳です")
+elif age >= 13 :
+    print("13歳以上です")
+else:
+    print("子供です")
+
+
+
+number = 4
+
+if number % 2 == 0 :
+    print("偶数です")
+else:
+    print("奇数です")
+
+
+
+score = 100
+
+if score == 100 :
+    print("神")
+elif score >=90:
+    print("S")
+elif score >= 70:
+    print("A")
+elif score >= 50:
+    print("B")
+else:
+    print("C")
+
+
+
+for i in range(11):
+    print(i)
+
+
+for i in range(6):
+    print(str(i)+"回目")
+
+
+fruits = ["りんご", "バナナ", "みかん"]
+
